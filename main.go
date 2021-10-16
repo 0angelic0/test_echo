9cebc8af7a7f5099a2ceab534ed3218babd55cba
0angelic0 <kungking.cpre@gmail.com>
2021-10-16 18:07:12 +0700

    Initial commit.

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+	e.GET("/users/:id", getUser)
+	e.GET("/users", getUsers, track)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func getUser(c echo.Context) error {
+	// User ID from path `users/:id`
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+func getUsers(c echo.Context) error {
+	return c.String(http.StatusOK, "/users")
+}
+
+// Route level middleware
+func track(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		fmt.Println("request to /users")
+		return next(c)
+	}
+}
